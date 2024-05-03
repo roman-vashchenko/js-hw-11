57774f0ceda4cd2f07a12968fb888c654b6aaa47
Roman Vashchenko <roman1991dp@gmail.com>
2024-05-03 23:13:59 +0300

    update

--- src/js/api-pixabay.js
+++ src/js/api-pixabay.js
@@ -17,4 +17,9 @@ export default class apiServicePixabay {
   incrementPage() {
     this.page += 1;
   }
+  getLatestData() {
+    return axios.get(
+      `https://pixabay.com/api/?key=33776129-06b0afe52f3ec0e98d1b43427&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=20`
+    );
+  }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,64 +1,88 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import ApiPixabayService from './api-pixabay';
 
 const form = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
+const loader = document.querySelector('.wrrap-loader');
 
 const apiPixabayService = new ApiPixabayService();
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
 
 form.addEventListener('submit', onSubmitForm);
 loadMoreBtn.addEventListener('click', onLoadMore);
 
 async function onSubmitForm(e) {
+  loader.style.display = 'block';
   e.preventDefault();
   apiPixabayService.resetPage();
-  gallery.innerHTML = '';
+  clearingPage(gallery);
   const formValue = e.currentTarget.elements.searchQuery.value.trim();
+  if (formValue === '') {
+    loader.style.display = 'none';
+    loadMoreBtn.style.display = 'none';
+    return;
+  }
   apiPixabayService.searchQuery = formValue;
   try {
     await apiPixabayService.getData().then(({ data }) => {
       const { totalHits, hits } = data;
-      console.log(data);
-      if (totalHits !== 0) {
+      if (hits.length < 40) {
+        renderMarkup(gallery, createMarkup(hits));
+        loadMoreBtn.style.display = 'none';
+        loader.style.display = 'none';
         Notify.info(`Hooray! We found ${totalHits} images.`);
-
-        gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
-        loadMoreBtn.disabled = false;
+      } else if (totalHits !== 0) {
+        Notify.info(`Hooray! We found ${totalHits} images.`);
+        renderMarkup(gallery, createMarkup(hits));
         apiPixabayService.incrementPage();
+        lightbox.refresh();
+        loadMoreBtn.style.display = 'block';
+        loader.style.display = 'none';
       } else {
+        loadMoreBtn.style.display = 'none';
+        loader.style.display = 'none';
         Notify.info(
           'Sorry, there are no images matching your search query. Please try again.'
         );
       }
-      console.log(data);
     });
   } catch (error) {
-    const message = error.message;
-    console.log(message);
-    Notify.failure(`${message}`);
+    Notify.failure(`${error.message}`);
   }
 
   form.reset();
 }
 
 async function onLoadMore() {
+  loader.style.display = 'block';
   try {
     await apiPixabayService.getData().then(({ data }) => {
       const { totalHits, hits } = data;
-      gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
+      renderMarkup(gallery, createMarkup(hits));
       apiPixabayService.incrementPage();
+      lightbox.refresh();
+      loader.style.display = 'none';
       if (totalHits < apiPixabayService.page * 40) {
-        loadMoreBtn.disabled = true;
+        apiPixabayService.getLatestData().then(({ data }) => {
+          loader.style.display = 'block';
+          const { totalHits, hits } = data;
+          renderMarkup(gallery, createMarkup(hits));
+          lightbox.refresh();
+          loadMoreBtn.style.display = 'none';
+          loader.style.display = 'none';
+        });
         return Notify.info(
           `We're sorry, but you've reached the end of search results.`
         );
       }
     });
   } catch (error) {
-    const message = error.message;
-    console.log(message);
-    Notify.failure(`${message}`);
+    loadMoreBtn.style.display = 'none';
+    loader.style.display = 'none';
+    Notify.failure(`${error.message}`);
   }
 }
 
@@ -74,7 +98,9 @@ function createMarkup(arr) {
         comments,
         downloads,
       }) => `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      <a href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" title="${tags}" />
+  </a>
   <div class="info">
     <p class="info-item">
       <b>Likes: ${likes}</b>
@@ -92,4 +118,12 @@ function createMarkup(arr) {
 </div>`
     )
     .join('');
+}
+
+function clearingPage(element) {
+  element.innerHTML = '';
+}
+
+function renderMarkup(element, arr) {
+  element.insertAdjacentHTML('beforeend', arr);
 }
